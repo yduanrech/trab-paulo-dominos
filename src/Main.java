a739375120da7f4ee96123687dfd63c1902a2206
Maria <mariafcknt@outlook.com>
2024-05-21 19:42:06 -0300

    add title

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import game.Jogo;
 
 public class Main {
     public static void main(String[] args) {
-        Jogo jogo = new Jogo(); //Cria uma nova instância do Jogo
-        jogo.iniciarPartida(); //Inicia a partida
-
+        System.out.println("--------------- DOMINÓ ---------------");
+        Jogo jogo = new Jogo(); // Cria uma nova instância do Jogo
+        jogo.iniciarPartida(); // Inicia a partida
     }
 }
